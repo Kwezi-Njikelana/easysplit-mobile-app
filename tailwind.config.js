fedaa6a438303a23760e551b5b802965150b1aa6
KweziNjikelana1 <Kwezinjiks@gmail.com>
2026-02-24 11:12:48 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./screens/**/*.{js,jsx,ts,tsx}",
+  ],
+  presets: [require("nativewind/preset")],
+
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: "#ecfdf5",
+          100: "#d1fae5",
+          500: "#10b981",
+          600: "#059669",
+          700: "#047857",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
